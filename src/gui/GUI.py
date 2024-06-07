0df9b944404958bcfea3defe092d56bba699f908
darkling-01 <barragan13al@gmail.com>
2024-06-06 22:57:30 -0700

    MainWindow has background-color, label, and changed the structure of code

--- src/gui/GUI.py
+++ src/gui/GUI.py
@@ -2,17 +2,30 @@ import sys
 from PyQt5 import QtWidgets, QtCore, QtGui
 
 
-# create the application object
-app = QtWidgets.QApplication(sys.argv)
-# create the form object
-widget = QtWidgets.QWidget()
-# set window size (width, height)
-widget.resize(1400, 900)
-# set the form title
-widget.setWindowTitle("Pneumonia")
-# show form
-widget.show()
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
 
-# run the program
-exit(app.exec())
+        # set background-color
+        self.setStyleSheet("background-color: #2c2e30")
+        self.setWindowTitle("Pneumonia Detection")
+        # set window size (0, 0, width, height)
+        self.setGeometry(0, 0, 1400, 900)
+
+        self.textTitle = QtWidgets.QLabel("Pneumonia Detection", self)
+        self.textTitle.setStyleSheet("QLabel { align-text: center; color: white; font-size: 22px;}")
+
+        # show form
+        self.show()
+
+
+if __name__ == "__main__":
+
+    # create the application object
+    app = QtWidgets.QApplication(sys.argv)
+    # create the instance of window
+    window_1 = MainWindow()
+
+    # run the program
+    exit(app.exec())
 
