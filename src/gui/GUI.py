97dc4e43b939eeaaab7fe9a1062b3c5da552936b
darkling-01 <barragan13al@gmail.com>
2024-06-07 14:25:36 -0700

    implemented fileDialog, created two additonal files, and imported pneumonia images

--- src/gui/GUI.py
+++ src/gui/GUI.py
@@ -1,8 +1,10 @@
 import sys
-from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 
-class MainWindow(QtWidgets.QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
@@ -12,17 +14,35 @@ class MainWindow(QtWidgets.QWidget):
         # set window size (0, 0, width, height)
         self.setGeometry(0, 0, 1400, 900)
 
-        self.textTitle = QtWidgets.QLabel("Pneumonia Detection", self)
-        self.textTitle.setStyleSheet("QLabel { align-text: center; color: white; font-size: 22px;}")
+        # Title
+        self.textTitle = QLabel("Pneumonia Detection", self)
+        self.textTitle.move(550, 30)
+        self.textTitle.resize(220, 30)
+        self.textTitle.setAlignment(Qt.AlignHCenter)
+        self.textTitle.setStyleSheet("color: white; font-size: 22px;")
 
-        # show form
+        self.UiComponents()
+
+        # showing all the widgets
         self.show()
+
+    def UiComponents(self):
+        button = QPushButton("Select A File", self)
+        # (x, y, width, height)
+        button.setGeometry(627, 100, 80, 30)
+        button.setStyleSheet("color: black; background-color: white;")
+        button.clicked.connect(self.FileDialog)
+
+    # create file dialog to open File Explorer
+
+    def FileDialog(self):
+        self.fileDialog = QFileDialog.getOpenFileName(self, "Pick An image", "home/", "Images (*.png *.xpm *.jpeg)")
 
 
 if __name__ == "__main__":
 
     # create the application object
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     # create the instance of window
     window_1 = MainWindow()
 
