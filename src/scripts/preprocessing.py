35a4bc5dde00dd04b6517afff621eedc07c61b57
darkling-01 <barragan13al@gmail.com>
2024-07-01 23:33:13 -0700

    revamping preprocesing.py with connection of using GUI.py

--- src/scripts/preprocessing.py
+++ src/scripts/preprocessing.py
@@ -9,35 +9,19 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
 
 
-labels = ['opacity', 'normal']
-# reading larger image size might slow down the process
+labels = ['normal', 'opacity']
 img_size = 150
-
-epochs = 14
-batchSize = 32
 
 
 def load_pneumonia_data(data_dir):
+    # empty object will store dataset(s) when compile
     data = []
+    # will loop through images in folders labeling them respectfully
     for label in labels:
-        path = os.path.join(data_dir, label)
+        PATH = os.path.join(data_dir, label)
         class_num = labels.index(label)
-        for img in os.listdir(path):
-
+        for img in os.listdir(data_dir):
             try:
-                # set images to grayscale
-                img_arr = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
-                resized_arr = cv2.resize(img_arr, (img_size, img_size))
-
-                data.append([resized_arr, class_num])
-
-            # if anything fails, the 'except' will print error message
-            except Exception as e:
-                print(e)
-    # convert images to array to easier in model
-    return np.array(data, dtype="object")
+                
 
 
-train = load_pneumonia_data(r"C:\Users\Alejandro Barragan\PycharmProjects\Pneumonia-Software\data\train")
-test = load_pneumonia_data(r"C:\Users\Alejandro Barragan\PycharmProjects\Pneumonia-Software\data\test")
-val = load_pneumonia_data(r"C:\Users\Alejandro Barragan\PycharmProjects\Pneumonia-Software\data\val")
