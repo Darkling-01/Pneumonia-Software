143f923e2b4a0b32fc914e2f3ed517fc4b0ba16d
darkling-01 <barragan13al@gmail.com>
2024-07-23 22:01:14 -0700

    modified the requirements and creating a short CNN class

--- src/scripts/model.py
+++ src/scripts/model.py
@@ -2,11 +2,20 @@ import tensorflow as tf
 from tensorflow import keras
 from keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from keras.models import Sequential, load_model
+from preprocessing import blur_train, blur_test, blur_val
 
-from preprocessing import blur_train, blur_test, blue_val
+
+class CNN(tf.keras.models):
+    def __int__(self):
+        super(CNN, self).__int__()
+        self.conv1 = Conv2D()
 
 
-# preparing images for machine learning model
-def encode_images(load_path):
-    
+# technique used to represent categorical data, such as our class labels. Each category is assigned a
+# unique binary code
+# e.g. NORMAL or OPACITY
+def encode_images(load_path, classes):
+    encoded = tf.keras.utils.to_categorical(load_path, classes)
+
+    return encoded
 
