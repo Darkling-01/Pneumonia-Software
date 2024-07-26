b8216d56d5908a7ffcd1e79d47103be585a932e4
darkling-01 <barragan13al@gmail.com>
2024-07-25 23:01:15 -0700

    created class for CNN with layers

--- src/scripts/model.py
+++ src/scripts/model.py
@@ -8,8 +8,20 @@ from preprocessing import blur_train, blur_test, blur_val
 class CNN(tf.keras.models):
     def __int__(self):
         super(CNN, self).__int__()
-        self.conv1 = Conv2D()
+        self.conv1 = Conv2D(32, 4, activation='relu', padding='same')
+        self.pooling = MaxPooling2D(2)
+        self.flatten = Flatten()
+        self.fc1 = Dense(120, activation='relu')
+        self.fc2 = Dense(2, activation='softmax')  # assuming 2 classes for the output
 
+    def call(self, inputs):
+        x = self.conv1(inputs)
+        x = self.pooling(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+
+        return self.fc2(x)
+    
 
 # technique used to represent categorical data, such as our class labels. Each category is assigned a
 # unique binary code
