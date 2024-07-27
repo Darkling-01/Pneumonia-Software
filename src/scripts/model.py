dd8dd52eb7c6d08069da8d93ed06d323484f0aa6
darkling-01 <barragan13al@gmail.com>
2024-07-26 22:29:41 -0700

    implemented compile and resizing of images but i recieve error messages of Numpy array to Tensor

--- src/scripts/model.py
+++ src/scripts/model.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 from tensorflow import keras
 from keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
-from keras.models import Sequential, load_model
+from keras.models import Sequential, Model
 from preprocessing import blur_train, blur_test, blur_val
 
 
-class CNN(tf.keras.models):
-    def __int__(self):
-        super(CNN, self).__int__()
+class CNN(Model):
+    def __init__(self):
+        super(CNN, self).__init__()
         self.conv1 = Conv2D(32, 4, activation='relu', padding='same')
         self.pooling = MaxPooling2D(2)
         self.flatten = Flatten()
@@ -16,18 +16,24 @@ class CNN(tf.keras.models):
 
     def call(self, inputs):
         x = self.conv1(inputs)
-        x = self.pooling(x)
+        x = self.pooling(x)     # pooling is used to shrink input images
         x = self.flatten(x)
         x = self.fc1(x)
 
         return self.fc2(x)
-    
+
 
 # technique used to represent categorical data, such as our class labels. Each category is assigned a
 # unique binary code
 # e.g. NORMAL or OPACITY
-def encode_images(load_path, classes):
-    encoded = tf.keras.utils.to_categorical(load_path, classes)
+def preprocess_images(images, target_size=(128, 128)):
+    images_resized = tf.image.resize(images, target_size)
+    images_normalized = images_resized / 255.0
 
-    return encoded
+    return images_normalized
 
+
+images_preprocessed = preprocess_images(blur_train)
+
+model = CNN()
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
