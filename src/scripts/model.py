23e2f5546b1b23eedf10c3eeec0c1ae24cebf8f0
darkling-01 <barragan13al@gmail.com>
2024-07-27 21:36:47 -0700

    i implemented a new CNN model but still stumble across Numpy arrat failing to convert

--- src/scripts/model.py
+++ src/scripts/model.py
@@ -8,19 +8,29 @@ from preprocessing import blur_train, blur_test, blur_val
 class CNN(Model):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = Conv2D(32, 4, activation='relu', padding='same')
-        self.pooling = MaxPooling2D(2)
+        # first convolutional layer
+        self.conv1 = Conv2D(32, 4, activation='relu', padding='same')   # 'same' with zero padding
+        # first pooling layer
+        self.pool1 = MaxPooling2D(2)
+        # second convolutional layer
+        self.conv2 = Conv2D(64, 4, activation='relu', padding='same')
+        # second pooling layer
+        self.pool2 = MaxPooling2D(2)
+        # Flatten layer to convert 2D feature maps to 1D
         self.flatten = Flatten()
-        self.fc1 = Dense(120, activation='relu')
-        self.fc2 = Dense(2, activation='softmax')  # assuming 2 classes for the output
+        # First fully connected (dense) layer
+        self.dense1 = Dense(120, activation='relu')
+        # Output layer with softmax activation for classification
+        self.dense2 = Dense(10, activation='softmax')
 
     def call(self, inputs):
         x = self.conv1(inputs)
-        x = self.pooling(x)     # pooling is used to shrink input images
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
         x = self.flatten(x)
-        x = self.fc1(x)
-
-        return self.fc2(x)
+        x = self.dense1(x)
+        return self.dense2(x)
 
 
 # technique used to represent categorical data, such as our class labels. Each category is assigned a
@@ -37,3 +47,5 @@ images_preprocessed = preprocess_images(blur_train)
 
 model = CNN()
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+
