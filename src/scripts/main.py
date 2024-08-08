c6cfc410906385eac9fad0bf12b9664feaeed949
darkling-01 <barragan13al@gmail.com>
2024-08-07 22:09:45 -0700

    main.py will now be used to run application

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,7 +1,14 @@
-from preprocessing import x_train, x_test, model, history
-
+import sys
+from src.gui import GUI
 
 if __name__ == "__main__":
 
-    print(history)
+    # create the application object
+    app = GUI.QtWidgets.QApplication(sys.argv)
+    # create the instance of window
+    window = GUI.MainWindow()
+    window.show()
+
+    # run the program
+    exit(app.exec())
 
