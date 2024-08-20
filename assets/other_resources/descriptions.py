862874d6b0e8d9eb903d2094c19e8c8b9e2f4ef9
darkling-01 <barragan13al@gmail.com>
2024-08-19 19:49:14 -0700

    created a seperate file.py to store my descriptions and stay organized

---
+++ assets/other_resources/descriptions.py
@@ -0,0 +1,4 @@
+text = "The purpose of this project is to be familiar with Qt5,\n" \
+       "learn new techniques in designing an application and\n" \
+       "understand how to complete a full-pipeline project,"
+
