523e9536880a8e7ad591bf0aa3f0a55ddd3c9e97
darkling-01 <barragan13al@gmail.com>
2024-08-27 21:54:44 -0700

    using the DRY method and minimizing repeated code into shared.py

--- src/gui/shared.py
+++ src/gui/shared.py
@@ -1,2 +1,28 @@
 # this file will share functionality modules
 
+from PyQt5.QtWidgets import QStackedWidget, QWidget, QVBoxLayout
+
+
+class SharedClass:
+    def __init__(self, parent=None):
+        super().__init__()
+
+        # initialize the stacked widget and layout
+        self.parent = parent
+        self.stacked_widget = QStackedWidget()
+        self.create_layout()
+
+    def create_layout(self):
+        # create and configure layout
+        self.central_widget = QWidget(self.parent)
+        self.main_layout = QVBoxLayout(self.central_widget)
+        self.main_layout.addWidget(self.stacked_widget)
+
+    def add_widget(self, widget):
+        # method to add widgets to the stacked widget
+        self.stacked_widget.addWidget(widget)
+
+    def set_current_widget(self, index):
+        # method to set the current widget in the stacked widget
+        self.stacked_widget.setCurrentWidget(index)
+
